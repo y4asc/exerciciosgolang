83594fe1f61d36633a0f3915cc5fa602fb48d4de
yasmim candido <yasmim.candido@aluno.senai.br>
2025-04-04 09:34:32 -0300

    vetores

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func main() {
+	greeting := "hello my friends!"
+	fmt.Println(strings.Contains(greeting, "dog"))
+	fmt.Println(strings.ReplaceAll(greeting, "my", "mine"))
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting, "my"))
+	fmt.Println(strings.Split(greeting, "!"))
+	ages := []int {50, 80, 10}
+	sort.Ints(ages)
+	fmt.Println(ages)
+index := sort.SearchInts(ages,50)
+fmt.Println(index)
+names :=[]string{"Caroline", "Maicon", "Diego"}
+sort.Strings(names)
+fmt.Println(names)
+fmt.Println(sort.SearchStrings(names, "caroline"))
+x:=0
+
+for x < 5 {
+	fmt.Println(x)
+	x++
+}
+
+for i:=0; i <5; i++{
+fmt.Println("for 2: ", i )
+}
+
+for i:=0; i <len(names); i++{
+	fmt.Println(names[i])
+}
+
+for index, value := range names {
+	fmt.Println ("o indice é", index, "e o valor", value)
+
+}
+
+
+for i:=0; i <len(ages); i++{
+	fmt.Println(ages[i])
+}
+
+
+for index, value := range ages{
+	fmt.Println ("o indice é", index, "e o valor", value)
+
+}
+
+superherois := []string{"deadpool", "homem aranha", "batman"}
+
+for index, value := range superherois {
+	fmt.Println("o numero do herói:", index, "o nome do herói", value)
+
+}
+
+for i:=0; i < len(superherois); i++{
+fmt.Println("o numero do herói", i, "e o herói", superherois[i])
+}
+}

da1a206181c6ce257be2d1f19d4f57c10d88cd5f
yasmim candido <yasmim.candido@aluno.senai.br>
2025-04-10 09:51:39 -0300

    <>de idade

--- main.go
+++ main.go
@@ -1,36 +1,21 @@
-
 package main
 
 import (
-"fmt"
-
+	"fmt"
 )
 
- func main(){
-ages:=45
-fmt.Println(ages<=50)
-fmt.Println(ages >=50)
-fmt.Println(ages ==50)
-fmt.Println(ages !=50)
+func main() {
+	var idade int
 
-if ages <30{
-    fmt.Println("menor que 30 anos")
-}else if ages <40{
- fmt.Println("menor que 40 anos ")
-}else {
-    fmt.Println("não é menor que 40 anos")
-}
-names := []string{"Isadora", "Yasmim","Trunks", "Cebolinha", " Martin","Yuri"}
+	
+	fmt.Print("Digite sua idade: ")
+	fmt.Scan(&idade)
 
-for index, value := range names {
-    if index== 1{
-        fmt.Println(" Continue apos a posição ", index, " e o valor ", value)
-        continue 
-    }
-    if index >2 {
-        fmt.Println("sair após", index)
-        break
-    }
-    fmt.Println("Valor:",value)
+	if idade < 18 {
+		fmt.Println("Você é menor de idade.")
+	} else if idade >= 18 && idade <= 60 {
+		fmt.Println("Você é adulto.")
+	} else {
+		fmt.Println("Você é idoso.")
+	}
 }
- }

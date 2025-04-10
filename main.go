1b3a2ff0f9f706f63a70fe111c80d4bdf176300a
yasmim candido <yasmim.candido@aluno.senai.br>
2025-04-10 15:56:31 -0300

    saldo

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
-	"fmt"
+"fmt"
 )
 
-func main() {
-	var idade int
+func main(){
+    var opcao string
+    var saldo int
+    var valor int
+    var valor2 int
+    var opcao2 string
+    fmt.Println("digite seu saldo (um valor inteiro positivo): ")
+    fmt.Scan(&saldo)
+    for {
+    fmt.Println("escolha 'sacar' ou 'depositar'")
+    fmt.Scan(&opcao)
+    if opcao == "sacar"{
+        fmt.Println("quanto você quer sacar? ")
+        fmt.Scan(&valor) }
 
-	
-	fmt.Print("Digite sua idade: ")
-	fmt.Scan(&idade)
-
-	if idade < 18 {
-		fmt.Println("Você é menor de idade.")
-	} else if idade >= 18 && idade <= 60 {
-		fmt.Println("Você é adulto.")
-	} else {
-		fmt.Println("Você é idoso.")
-	}
+     if valor <= saldo && opcao == "sacar" {
+        fmt.Println("saldo suficiente")
+        saldo = saldo - valor
+        fmt.Println("seu saldo atual é ", saldo)
+        fmt.Println("se você quiser continuar, digite 'continuar', se quiser sair digite 'sair' ")
+        fmt.Scan(&opcao2)
+    } else if valor > saldo && opcao == "sacar" {
+        fmt.Println("saldo insuficiente, digite 'continuar' para tentar denovo ou 'sair' para terminar")
+        fmt.Scan(&opcao2)
+    }
+    if opcao == "depositar"{
+        fmt.Println("quanto você quer depositar?")
+        fmt.Scan(&valor2)
+        saldo = saldo + valor2
+fmt.Println("seu saldo atual é ", saldo)
+   fmt.Println("se você quiser continuar, digite 'continuar', se quiser sair digite 'sair' ")
+   fmt.Scan(&opcao2)
+    }
+    if opcao2 == "continuar" {
+        continue
+    } else if opcao2 == "sair" {
+        break
+    }
+}
 }

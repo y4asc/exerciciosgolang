6953db2ab0b56a55e2f7ac9e2d80ece0c426b180
yasmim candido <yasmim.candido@aluno.senai.br>
2025-04-11 13:47:14 -0300

    saldo2

--- main.go
+++ main.go
@@ -1,18 +1,49 @@
-package main
+package main 
 
-import (
-"fmt"
-)
+import ( 
+    "fmt" 
+) 
 
-func sayGreeting(nome string){
-  fmt.Println("Ola !", nome)
-    
-}
-func addNumber(numero1 int, numero2 int)int{
-    return numero1 + numero2
-}
-func main() {
-    sayGreeting("Juvelino")
-    resultado:= addNumber(10,20)
-    fmt.Println(resultado)
-}
+func main() { 
+    var saldo float32
+    fmt.Println("Insira o valor inicial:") 
+    fmt.Scan(&saldo) 
+    escolherOperacao(&saldo) 
+} 
+func escolherOperacao(saldo *float32) { 
+    var opcao int 
+    fmt.Println("Você deseja sacar (1), depositar (2) ou encerrar a sessão (3)?") 
+    fmt.Scan(&opcao) 
+    switch opcao { 
+    case 1: 
+        sacar(saldo) 
+    case 2: 
+        depositar(saldo) 
+    case 3: 
+        encerrar() 
+    default: 
+        fmt.Println("Opção inválida.") 
+    } 
+} 
+func depositar(saldo *float32) { 
+    var valor float32 
+    fmt.Println("Qual valor deseja depositar?") 
+    fmt.Scan(&valor) 
+    *saldo += valor 
+    fmt.Println("Seu saldo atual é:", *saldo) 
+} 
+func sacar(saldo *float32) { 
+    var valor float32 
+    fmt.Println("Qual valor deseja sacar?") 
+    fmt.Scan(&valor) 
+    if valor > *saldo { 
+        fmt.Println("Saldo insuficiente.") 
+    } else { 
+        *saldo -= valor 
+        fmt.Println("Seu saldo atual é:", *saldo) 
+    } 
+}  
+func encerrar() { 
+
+    fmt.Println("Sessão encerrada.") 
+} 

dd0804fc70f4f82c2d53ded597e8bac3a3aeab15
yasmim candido <yasmim.candido@aluno.senai.br>
2025-04-17 08:47:36 -0300

    resultado

--- main.go
+++ main.go
@@ -1,49 +1,35 @@
-package main 
+package main
 
-import ( 
-    "fmt" 
-) 
+import (
+    "fmt"
+)
 
-func main() { 
-    var saldo float32
-    fmt.Println("Insira o valor inicial:") 
-    fmt.Scan(&saldo) 
-    escolherOperacao(&saldo) 
-} 
-func escolherOperacao(saldo *float32) { 
-    var opcao int 
-    fmt.Println("Você deseja sacar (1), depositar (2) ou encerrar a sessão (3)?") 
-    fmt.Scan(&opcao) 
-    switch opcao { 
-    case 1: 
-        sacar(saldo) 
-    case 2: 
-        depositar(saldo) 
-    case 3: 
-        encerrar() 
-    default: 
-        fmt.Println("Opção inválida.") 
-    } 
-} 
-func depositar(saldo *float32) { 
-    var valor float32 
-    fmt.Println("Qual valor deseja depositar?") 
-    fmt.Scan(&valor) 
-    *saldo += valor 
-    fmt.Println("Seu saldo atual é:", *saldo) 
-} 
-func sacar(saldo *float32) { 
-    var valor float32 
-    fmt.Println("Qual valor deseja sacar?") 
-    fmt.Scan(&valor) 
-    if valor > *saldo { 
-        fmt.Println("Saldo insuficiente.") 
-    } else { 
-        *saldo -= valor 
-        fmt.Println("Seu saldo atual é:", *saldo) 
-    } 
-}  
-func encerrar() { 
+func dividir(dividendo int, divisor int)(int, string){
+    if divisor == 0 {
+        return 0, "Erro na divisão por zero"
+    }
+    return dividendo / divisor, "Sem erro" 
 
-    fmt.Println("Sessão encerrada.") 
-} 
+  }
+  func operacaoBasica(a int, b int ) (int , int ,int ){
+    soma := a+b
+    multiplicacao := a * b
+    subtracao := a - b 
+    return soma , multiplicacao , subtracao
+
+  }
+
+  func main(){
+    resultado, erro := dividir(10,2)
+
+    if erro != "Sem erro"{
+        fmt.Println(erro)
+    }else {
+        fmt.Println("O resultado da  divisão é :", resultado)
+    }
+    soma , mult , sub := operacaoBasica(10,2)
+    fmt.Println(soma)
+    fmt.Println(mult)
+    fmt.Println(sub)
+
+  }

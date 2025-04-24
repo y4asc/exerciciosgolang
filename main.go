c35204a0991986009e2985df1e9741d70b84619c
yasmim candido <yasmim.candido@aluno.senai.br>
2025-04-24 13:25:14 -0300

    nome e sobrenome

--- main.go
+++ main.go
@@ -1,28 +1,13 @@
-package main
+  package main
 
-import (
-    "fmt"
-)
+  import "fmt"
 
-func dadosPessoa(nome string, idade int) (int, string) {
-    if idade > 18 {
-        fmt.Println("Você é maior de idade")
-    }else
-    if idade <= 17{
-        fmt.Println("Você é menor de idade") 
-    }
-    return idade, nome
-  } 
+  func pegarNome()(string, string){
+return "Barry", "Aallen"
+  }
   
-
   func main(){
-    var idade int 
-    var nome string
-
-    fmt.Println("Digite sua idade:")
-    fmt.Scan(&idade)
-    fmt.Println("Sua idade é: ", idade)
-    fmt.Println("Qual o seu nome?")
-    fmt.Scan(&nome)
-     dadosPessoa(nome, idade)
+nome, sobrenome := pegarNome()
+fmt.Println(nome)
+fmt.Println(sobrenome)
   }

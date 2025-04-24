f50073fa9e60ec7014fde45e262f010476839ec7
yasmim candido <yasmim.candido@aluno.senai.br>
2025-04-24 14:28:50 -0300

    cantina atualizada

--- main.go
+++ main.go
@@ -3,20 +3,23 @@ package main
 import "fmt"
 
 func main() {
-  
-  estoque := map[string]int{
-        "COXINHA":       10,
-        "PÃO DE QUEIJO": 15,
-        "REFRIGERANTE":  20,
+
+    estoque := map[string]int{
+        "Coxinha":      10,
+        "Pão de Queijo": 15,
+        "Refrigerante": 20,
     }
 
-
-    estoque["COXINHA"] -= 2    
-    estoque["PÃO DE QUEIJO"]-- 
-
- 
-    fmt.Println("ESTOQUE ATUALIZADO:")
+    fmt.Println("Estoque inicial:")
     for produto, quantidade := range estoque {
         fmt.Printf("%s: %d\n", produto, quantidade)
     }
-}
+
+    estoque["Coxinha"] -= 2
+    estoque["Pão de Queijo"] -= 1
+
+    fmt.Println("\nEstoque atualizado:")
+    for produto, quantidade := range estoque {
+        fmt.Printf("%s: %d\n", produto, quantidade)
+    }
+  }
